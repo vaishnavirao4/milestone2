cde6746f1d004ed1d8ec4d0fbd94a503408cc919
vaishnavirao4 <vsanthap@my.centennialcollege.ca>
2022-11-06 13:30:20 -0500

    login page

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, Button, TextInput, Text, Image, View, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      
+      username: '',
+      password: '',
+    };
+  }
+  
+  onLogin() {
+    const { username, password } = this.state;
+
+    Alert.alert('Credentials are valid');
+  }
+
+  render() {
+    return (
+      
+    
+      <View style={styles.container}>
+         <View style={styles.image}>
+        <Image source={require('../helpinghands/assets/logo.png')}></Image>
+        </View>
+    <Text style={styles.heading}>Helping Hands</Text>
+  
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.buttonStyle}
+          onPress={this.onLogin.bind(this)}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f3e0fb',
   },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+  heading: {
+    fontSize: 28,
+    width: 196,
+    height: 100,
+    justifyContent: 'center',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: '#4a5e74',
+  },
+  buttonStyle: {
+    color: '#4a5e74'
+  },
+image: {
+marginTop: 5,
+width: 50,
+height: 100,
+alignSelf: 'center',
+
+},
+  
 });
